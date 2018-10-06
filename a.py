c6dd8ee3127be3919dc6f0084c99631f65109d2b
srimaruthi001 <srimaruthi001.gmail.com>
2018-10-06 18:14:48 -0400

    first change

--- a.py
+++ a.py
@@ -1,9 +1,1 @@
-import math
-c=50
-h=30
-value = []
-items=[x for x in input("Please entet seq of vallues").split(',')'
-for d in items:
-    value.append(stsr(int(round(math.sqrt((2 * c*d)/h)))))
-
-print( (','.join(value))
+print("this is a sample file "
